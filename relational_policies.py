73d5a74d5d8448cf65aed9a238a39970f66ab76f
gyh75520 <734468130@qq.com>
2019-11-19 18:57:43 +0800

    add RelationalLstmPolicy

--- relational_policies.py
+++ relational_policies.py
@@ -1,12 +1,13 @@
-from stable_baselines.common.policies import ActorCriticPolicy, mlp_extractor
+from stable_baselines.common.policies import ActorCriticPolicy, RecurrentActorCriticPolicy, mlp_extractor
 from stable_baselines.a2c.utils import linear
 import tensorflow as tf
-from utils import get_coor, MHDPA, residual_block, rrl_cnn
+from utils import get_coor, MHDPA, residual_block, rrl_cnn, boxworld_cnn
+from stable_baselines.a2c.utils import batch_to_seq, seq_to_batch, lstm
 
 
 class RelationalPolicy(ActorCriticPolicy):
     def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch, reuse=False, net_arch=None,
-                 act_fun=tf.nn.relu, cnn_extractor=rrl_cnn, feature_extraction="cnn", **kwargs):
+                 act_fun=tf.nn.relu, cnn_extractor=boxworld_cnn, feature_extraction="cnn", **kwargs):
         super(RelationalPolicy, self).__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, reuse=reuse,
                                                scale=(feature_extraction == "cnn"))
         self._kwargs_check(feature_extraction, kwargs)
@@ -60,3 +61,64 @@ class RelationalPolicy(ActorCriticPolicy):
 
     def value(self, obs, state=None, mask=None):
         return self.sess.run(self.value_flat, {self.obs_ph: obs})
+
+
+class RelationalLstmPolicy(RecurrentActorCriticPolicy):
+    recurrent = True
+
+    def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch, n_lstm=256, reuse=False, layers=None,
+                 net_arch=None, act_fun=tf.tanh, cnn_extractor=boxworld_cnn, layer_norm=False, feature_extraction="cnn",
+                 **kwargs):
+        # state_shape = [n_lstm * 2] dim because of the cell and hidden states of the LSTM
+        super(RelationalLstmPolicy, self).__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch,
+                                                   state_shape=(2 * n_lstm, ), reuse=reuse,
+                                                   scale=(feature_extraction == "cnn"))
+
+        self._kwargs_check(feature_extraction, kwargs)
+
+        with tf.variable_scope("model", reuse=reuse):
+            extracted_features = cnn_extractor(self.processed_obs, **kwargs)
+            print('extracted_features', extracted_features)
+            coor = get_coor(extracted_features)
+            # [B,Height,W,D+2]
+            entities = tf.concat([extracted_features, coor], axis=3)
+            print('entities:', entities)
+            # [B,H*W,num_heads,Deepth=D+2]
+            MHDPA_output, weights = MHDPA(entities, "extracted_features", num_heads=2)
+            print('MHDPA_output', MHDPA_output)
+            self.attention = weights
+            # [B,H*W,num_heads,Deepth]
+            residual_output = residual_block(entities, MHDPA_output)
+            print('residual_output', residual_output)
+
+            # max_pooling
+            residual_maxpooling_output = tf.reduce_max(residual_output, axis=[1])
+            print('residual_maxpooling_output', residual_maxpooling_output)
+
+            input_sequence = batch_to_seq(residual_maxpooling_output, self.n_env, n_steps)
+            masks = batch_to_seq(self.dones_ph, self.n_env, n_steps)
+            rnn_output, self.snew = lstm(input_sequence, masks, self.states_ph, 'lstm1', n_hidden=n_lstm,
+                                         layer_norm=layer_norm)
+            rnn_output = seq_to_batch(rnn_output)
+            value_fn = linear(rnn_output, 'vf', 1)
+
+            self._proba_distribution, self._policy, self.q_value = \
+                self.pdtype.proba_distribution_from_latent(rnn_output, rnn_output)
+
+        self._value_fn = value_fn
+
+        self._setup_init()
+
+    def step(self, obs, state=None, mask=None, deterministic=False):
+        if deterministic:
+            return self.sess.run([self.deterministic_action, self.value_flat, self.snew, self.neglogp],
+                                 {self.obs_ph: obs, self.states_ph: state, self.dones_ph: mask})
+        else:
+            return self.sess.run([self.action, self.value_flat, self.snew, self.neglogp],
+                                 {self.obs_ph: obs, self.states_ph: state, self.dones_ph: mask})
+
+    def proba_step(self, obs, state=None, mask=None):
+        return self.sess.run(self.policy_proba, {self.obs_ph: obs, self.states_ph: state, self.dones_ph: mask})
+
+    def value(self, obs, state=None, mask=None):
+        return self.sess.run(self.value_flat, {self.obs_ph: obs, self.states_ph: state, self.dones_ph: mask})
