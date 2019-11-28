:bug: MHDPA scaling factor

env/gym-box-world/gym_boxworld/envs/Box_world_rand_env.py
@@ -78,7 +78,9 @@ class BoxWoldRandEnv(gym.Env):
 
         # set observation space
         self.box_size = 14
-        self.obs_shape = [self.box_size * 10, self.box_size * 10, 3]
+        # scale
+        gs = 10
+        self.obs_shape = [self.box_size * gs, self.box_size * gs, 3]
         self.observation_space = spaces.Box(low=0, high=255, shape=self.obs_shape, dtype=np.uint8)
 
         # initialize system

utils.py
@@ -31,10 +31,10 @@ def simple_cnn(scaled_images, **kwargs):
     """
     activ = tf.nn.relu
     print('scaled_images', scaled_images)
-    layer_1 = activ(conv(scaled_images, 'c1', n_filters=24, filter_size=1, stride=1, init_scale=np.sqrt(2), **kwargs))
-    # layer_2 = activ(conv(layer_1, 'c2', n_filters=24, filter_size=2, stride=2, init_scale=np.sqrt(2), **kwargs))
-    print('layer_1', layer_1)
-    return layer_1
+    layer_1 = activ(conv(scaled_images, 'c1', n_filters=12, filter_size=2, stride=2, init_scale=np.sqrt(2), **kwargs))
+    layer_2 = activ(conv(layer_1, 'c2', n_filters=24, filter_size=2, stride=2, init_scale=np.sqrt(2), **kwargs))
+    print('layer_2', layer_2)
+    return layer_2
 
 
 def rrl_cnn(scaled_images, **kwargs):
@@ -119,7 +119,7 @@ def MHDPA(input_tensor, scope, num_heads):
         total_size = qkv_size * num_heads
 
         # Denote N = last_num_height * last_num_width
-        N = last_num_height * last_num_width
+        # N = last_num_height * last_num_width
         # [B*N,Deepth]
         extracted_features_reshape = tf.reshape(input_tensor, [-1, last_num_features])
         # [B*N,F]
@@ -139,7 +139,7 @@ def MHDPA(input_tensor, scope, num_heads):
         # q *= qkv_size ** -0.5
         # [B,H,N,N]
         dot_product = tf.matmul(q, k, transpose_b=True)
-        dot_product = dot_product * (N**-0.5)
+        dot_product = dot_product * (query_size**-0.5)
         weights = tf.nn.softmax(dot_product)
         # [B,H,N,V]
         output = tf.matmul(weights, v)